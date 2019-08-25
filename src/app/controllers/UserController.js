4c9cc75cff626cbed96f5bd35d5bd159d303a535
Pedro Nascimento <nennogabriel@gmail.com>
2019-08-25 20:24:20 -0300

    feat: Session Controller -> jwt token

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,6 +2,10 @@ import User from '../models/Users';
 
 class UserControler {
   async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
     const { id, name, email, provider } = await User.create(req.body);
     return res.json({ id, name, email, provider });
   }
