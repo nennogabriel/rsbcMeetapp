bb4cc189f56f17c96e6a5113f08f813897248e87
Pedro Nascimento <nennogabriel@gmail.com>
2019-08-31 11:58:27 -0300

    chore: data validation

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -6,6 +6,7 @@ import {
   endOfDay,
 } from 'date-fns';
 import { Op } from 'sequelize';
+import * as Yup from 'yup';
 import Meetup from '../models/Meetup';
 import User from '../models/User';
 
@@ -41,6 +42,17 @@ class MeetupController {
   }
 
   async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+      file_id: Yup.number().required(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const meetupHour = startOfHour(parseISO(req.body.date));
     if (isBefore(meetupHour, new Date())) {
       return res.status(400).json({
